cf770400c5ec6fe25955ed4760d25524f46ad069
Josmary Bermudez <josmary.bermudez@wearepsh.com>
2024-05-06 00:30:21 -0300

    test: removing troubling file

--- src/app/components/usuarios-gestionar/usuarios-gestionar.component.ts
+++ src/app/components/usuarios-gestionar/usuarios-gestionar.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 @Component({
   selector: 'app-usuarios-gestionar',
   templateUrl: './usuarios-gestionar.component.html',
-  styleUrls: ['./usuarios-gestionar.component.css']
 })
 export class UsuariosGestionarComponent implements OnInit {
 
